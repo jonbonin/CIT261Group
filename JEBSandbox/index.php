e6b6583cee83307d3020943e3321f01b9c36abbe
JONBONIN\Jonathan <jon.e.bonin@gmail.com>
2016-09-23 13:20:46 -0600

    created index file for webpage access

---
+++ JEBSandbox/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/head.php'; ?>
+        <title>
+            Home | mobileappdevelopment.com
+        </title>
+    </head>
+    <body>
+        <div>
+            <header role="banner">
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
+            </header>
+            <div?
+                <main role="main">
+                    <div>
+                        <h1>Page Heading yolo</h1>
+                        <!--The main content of the page will go here-->
+                    </div>
+                </main>
+            </div>
+            <aside role="complementary">
+                <!--This is info pertaining to the website as a whole-->
+            </aside>
+            <footer role="contentinfo">
+                <div>
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'; ?>
+                    <?php echo 'last update: ' . date('F j, Y', getlastmod()) ?>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
