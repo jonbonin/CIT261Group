ce2f6b3f0bf261bc66298811f541e5d8a116b950
JONBONIN\Jonathan <jon.e.bonin@gmail.com>
2016-09-23 17:38:22 -0600

    changes

--- JEBSandbox/xmlhttpRequest.php
+++ JEBSandbox/xmlhttpRequest.php
@@ -7,7 +7,7 @@
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/CIT261Group/JEBSandbox/modules/head.php'; ?>
         <script>
             function loadJSON() {
-                var data = "http://www.w3schools.com/json/myTutorials.txt";
+                var data = "http://services.groupkt.com/country/get/all";
 //                http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=44db6a862fba0b067b1930da0d769e98
 //                old url used
                 var http_request = new XMLHttpRequest();
@@ -16,11 +16,12 @@
                     if (http_request.readyState == 4) {
                         console.log(http_request.responseText);
 //                        Javascript function JSON.parse to parse JSON data
-                        var jsonObj = JSON.parse(http_request.responseText);
+                        var countryAbrev = JSON.parse(http_request.responseText);
 
-//                       jsonObj variable now contains the data structure and can be accessed as jsonObj.name and jsonObj.country.
-                        document.getElementById("Display").innerHTML = jsonObj.display;
-                        document.getElementById("URL").innerHTML = jsonObj.weather[0].url;
+//                       countryAbrev variable now contains the data structure and can be accessed as countryAbrev.name and countryAbrev.country.
+                        document.getElementById("DisplayCountry").innerHTML = countryAbrev.RestResponse.result[0].name;
+                        document.getElementById("TwoLetter").innerHTML = countryAbrev.RestResponse.result[0].alpha2_code;
+                        document.getElementById("ThreeLetter").innerHTML = countryAbrev.RestResponse.result[0].alpha3_code;
                     }
                 };
 
@@ -49,10 +50,11 @@
             In his situation, it is already in JSON, so we are taking it out of the JSON text, and putting it into normal English.
         </p>
         <br>
-        <br> 
-        <p id="DisplayCity">City will display here</p>
-        <p id="DisplayWeather">Weather will display here</p>
         <button type = "button" onclick = "loadJSON()"> Display Details </button>
+        
+        <p id="DisplayCountry">Country will display here</p>
+        <p id="TwoLetter">Two letter Code will display here</p>
+        <p id="ThreeLetter">Three letter Code will display here</p>
         <!--resource that helped me understand:https://www.youtube.com/watch?v=SjLJ8Dyuywg-->
     </body>
 </html>
